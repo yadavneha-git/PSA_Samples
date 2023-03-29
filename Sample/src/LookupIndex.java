e56807a5022fe65a62812dd513c2a61e8bfd3d0b
yadavneha-git <yadav.neha1@northeastern.edu>
2023-03-29 15:40:37 -0700

    Samples of programs and algorithms

---
+++ Sample/src/LookupIndex.java
@@ -0,0 +1,18 @@
+/**
+ *  The {@code LookupIndex} class provides a data-driven client for reading in a
+ *  key-value pairs from a file; then, printing the values corresponding to the
+ *  keys found on standard input. Keys are strings; values are lists of strings.
+ *  The separating delimiter is taken as a command-line argument. This client
+ *  is sometimes known as an <em>inverted index</em>.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/35applications">Section 3.5</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class LookupIndex {
+
+
+}
+
